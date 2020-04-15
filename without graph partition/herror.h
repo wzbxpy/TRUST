0cb165fe1d03306ecf503a9318b54db27b48a372
wzbxpy <958215422@qq.com>
2020-04-15 14:52:37 +0000

    delete file

--- without graph partition/herror.h
+++
@@ -1,8 +0,0 @@
-static void HandError( cudaError_t err, const char *file, int line   ) {
-        if (err != cudaSuccess) {
-                    printf( "\n%s in %s at line %d\n", \
-                                             cudaGetErrorString( err   ), file, line );
-                             exit( EXIT_FAILURE   );
-                                 }
-         }
- #define HRR( err   ) (HandError( err, __FILE__, __LINE__   ))
